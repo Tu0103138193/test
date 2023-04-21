e78d11b491da430a8a894c20514f01184a5a55d0
Ngoc Anh cau hinh <anhcu2101@gmail.com>
2023-04-21 21:50:30 +0700

    ten pro trong Util , Validate dữ liệu trước khi thêm

---
+++ QuanLyBanMaytinh/Util/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanMaytinh.Util
+{
+    class Utils
+    {
+        /*Ten proc */
+        public static string insertNhanVien = "InsertNhanVien";
+        public static string UpdateNhanVien = "UpdateNhanVien";
+        public static string DeleteNhanVien = "DeleteNhanVien";
+        public static string EditNhanVien = "EditNhanVien";
+        public static string ReportNhanVien = "ReportNhanVien";
+        public static string SearchNhanVien = "SearchNhanVien";
+
+    }
+}
---
+++ QuanLyBanMaytinh/Validate/Validates.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanMaytinh.Validate
+{
+    class Validates
+    {
+        public bool checkEmpty(string strinfo)
+        {
+            if (strinfo.Trim().Equals(""))
+                return false;
+
+        return true;
+        }
+    }
+}
